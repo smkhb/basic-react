06a3600a6639258e599066071d9bb1ae8f4023ba
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2025-04-19 20:03:07 -0300

    Adds Avatar component and updates usage in comments
    Introduces a new Avatar component for consistent user profile images.
    
    Replaces direct image usage in Comment, Post, and Sidebar components with the Avatar component for improved styling and flexibility.
    
    Removes redundant CSS for avatars from other component stylesheets.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from "@phosphor-icons/react";
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -10,7 +11,7 @@ export function Sidebar() {
           src="https://images.unsplash.com/photo-1744868052130-325034c971b9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D"
         />
         <div className={styles.profile}>
-          <img src="https://github.com/smkhb.png" className={styles.avatar} />
+          <Avatar src="https://github.com/smkhb.png" />
           <strong>John Doe</strong>
           <span>Web Developer</span>
         </div>
